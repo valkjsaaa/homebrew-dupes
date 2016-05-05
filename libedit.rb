b823e3d6d00c8a91580daecc98a3825aa738984a
Shaun Jackman <sjackman@gmail.com>
2016-05-05 14:34:46 -0700

    libedit: update 20150325-3.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-dupes#10.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- libedit.rb
+++ libedit.rb
@@ -10,6 +10,7 @@ class Libedit < Formula
     sha256 "f2412c8ce6d8651b32f52389942b8e7c777e0cb6cc9237d2c55a6b7dd0392143" => :yosemite
     sha256 "03a8764730a83c6438e25aeee88c1b68c4b69fa9d257f7154f80ccc15c403a5c" => :mavericks
     sha256 "302a08f178639005bbd9ca6732775ef36f98371772d95a66373f30d3856e812a" => :mountain_lion
+    sha256 "13bdbcf0f595d4ebf9d22a54cf4da54965e0ec7c02072be572f06e66c773f9bc" => :x86_64_linux
   end
 
   keg_only :provided_by_osx
