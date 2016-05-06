f125d7e08636c1b6312b1afbcb0f41f58278a406
Shaun Jackman <sjackman@gmail.com>
2016-05-05 23:33:17 -0700

    unzip: update 6.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-dupes#35.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- unzip.rb
+++ unzip.rb
@@ -5,6 +5,11 @@ class Unzip < Formula
   version "6.0"
   sha256 "036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fcda165cef76bb86bc74c5435a4a46341695d49c04ff74da361c9be0d594fe9b" => :x86_64_linux
+  end
+
   keg_only :provided_by_osx
 
   def install
