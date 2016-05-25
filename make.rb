068a3e7183ff07949863407bc6cda7af81a5c610
Shaun Jackman <sjackman@gmail.com>
2016-05-25 15:12:37 -0700

    make: update 4.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-dupes#39.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- make.rb
+++ make.rb
@@ -9,6 +9,7 @@ class Make < Formula
     sha256 "e6f872ec47d2157a7d5ae3f6854128844f8e305fb7ee54dd98c23fa7c4bc1561" => :el_capitan
     sha256 "c5cc8218cc2f379bdb8f5c5fb3847ac4a952f952cd6169b232f68d2cd99527f5" => :yosemite
     sha256 "04dd0b10205bce4ed7e0c58fea3363a72fe7116346fa0ae7fe7fa4116832f60a" => :mavericks
+    sha256 "e38903e23b2d3f87208d185a150dc3c5f6ab95965fafc2ee56a40d70ae5b386c" => :x86_64_linux
   end
 
   option "with-default-names", "Do not prepend 'g' to the binary"
