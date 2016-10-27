658361e663fc9593f95a4251aa7fbcc018fe18cb
ilovezfs <ilovezfs@icloud.com>
2016-10-27 00:00:44 -0700

    file-formula 5.29
    patch ensures the libmagic formula's library is used
    
    Closes #672.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- file-formula.rb
+++ file-formula.rb
@@ -2,10 +2,9 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "ftp://ftp.astron.com/pub/file/file-5.28.tar.gz"
-  mirror "https://fossies.org/linux/misc/file-5.28.tar.gz"
-  sha256 "0ecb5e146b8655d1fa84159a847ee619fc102575205a0ff9c6cc60fc5ee2e012"
-
+  url "ftp://ftp.astron.com/pub/file/file-5.29.tar.gz"
+  mirror "https://fossies.org/linux/misc/file-5.29.tar.gz"
+  sha256 "ea661277cd39bf8f063d3a83ee875432cc3680494169f952787e002bdd3884c0"
   head "https://github.com/file/file.git"
 
   bottle do
@@ -19,7 +18,11 @@ class FileFormula < Formula
 
   depends_on "libmagic"
 
+  patch :DATA
+
   def install
+    ENV.prepend "LDFLAGS", "-L#{Formula["libmagic"].opt_lib} -lmagic"
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install-exec"
@@ -31,3 +34,26 @@ class FileFormula < Formula
     system "#{bin}/file", test_fixtures("test.mp3")
   end
 end
+
+__END__
+diff --git a/src/Makefile.in b/src/Makefile.in
+index b6eeb20..4a7ae91 100644
+--- a/src/Makefile.in
++++ b/src/Makefile.in
+@@ -150,7 +150,6 @@ libmagic_la_LINK = $(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) \
+ PROGRAMS = $(bin_PROGRAMS)
+ am_file_OBJECTS = file.$(OBJEXT)
+ file_OBJECTS = $(am_file_OBJECTS)
+-file_DEPENDENCIES = libmagic.la
+ AM_V_P = $(am__v_P_@AM_V@)
+ am__v_P_ = $(am__v_P_@AM_DEFAULT_V@)
+ am__v_P_0 = false
+@@ -352,7 +351,7 @@ libmagic_la_LDFLAGS = -no-undefined -version-info 1:0:0
+ @MINGW_TRUE@MINGWLIBS = -lgnurx -lshlwapi
+ libmagic_la_LIBADD = $(LTLIBOBJS) $(MINGWLIBS)
+ file_SOURCES = file.c
+-file_LDADD = libmagic.la
++file_LDADD = $(LDADD)
+ CLEANFILES = magic.h
+ EXTRA_DIST = magic.h.in
+ HDR = $(top_srcdir)/src/magic.h.in
